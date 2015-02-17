971934dec921c148df007fab4b4535ab6927ade4
jane0828 <janewu0828@gmail.com>
2015-02-17 22:07:56 +0800

    add fun

--- AEStest2/src/com/example/aestest2/MainActivity.java
+++ AEStest2/src/com/example/aestest2/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.aestest2;
 
+import java.io.ByteArrayOutputStream;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 import android.support.v7.app.ActionBarActivity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,7 +21,51 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();  
+		Bitmap bm = null;
+		bm.compress(Bitmap.CompressFormat.PNG, 100, baos); // bm is the bitmap object   
+		byte[] b = baos.toByteArray();  
+
+		byte[] keyStart = "this is a key".getBytes();
+		KeyGenerator kgen;
+		try {
+			kgen = KeyGenerator.getInstance("AES");
+			SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+			sr.setSeed(keyStart);
+			kgen.init(128, sr); // 192 and 256 bits may not be available
+			SecretKey skey = kgen.generateKey();
+			byte[] key = skey.getEncoded();    
+
+			// encrypt
+			byte[] encryptedData = encrypt(key,b);
+			// decrypt
+			byte[] decryptedData = decrypt(key,encryptedData);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO 自動產生的 catch 區塊
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO 自動產生的 catch 區塊
+			e.printStackTrace();
+		}
+		
 	}
+	
+	private static byte[] encrypt(byte[] raw, byte[] clear) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        byte[] encrypted = cipher.doFinal(clear);
+        return encrypted;
+    }
+
+    private static byte[] decrypt(byte[] raw, byte[] encrypted) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+        byte[] decrypted = cipher.doFinal(encrypted);
+        return decrypted;
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
