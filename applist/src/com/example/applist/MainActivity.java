f42dc28a639cbc77369928e467f3698e3fad1951
jane0828 <janewu0828@gmail.com>
2015-05-10 15:30:57 +0800

    add

--- applist/src/com/example/applist/MainActivity.java
+++ applist/src/com/example/applist/MainActivity.java
@@ -1,16 +1,81 @@
 package com.example.applist;
 
+import java.util.List;
+
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class MainActivity extends ActionBarActivity {
+
+	// 系统中所有的app信息
+	private List<ResolveInfo> apps;
+
+	// 用九宫格视图呈现所有app
+	private GridView apps_list;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		apps_list = (GridView) findViewById(R.id.apps_list);
+		loadApps();
+	}
+
+	private void loadApps() {
+		// 获取系统所有app信息
+		Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		new ImageView(MainActivity.this);
+		apps = getPackageManager().queryIntentActivities(mainIntent, 0);
+		// 把信息设置到适配器中
+		apps_list.setAdapter(new AppsAdapter());
+	}
+
+	public class AppsAdapter extends BaseAdapter {
+
+		public AppsAdapter() {
+
+		}
+
+		@Override
+		public int getCount() {
+			return apps.size();
+		}
+
+		@Override
+		public Object getItem(int i) {
+			return apps.get(i);
+		}
+
+		@Override
+		public long getItemId(int i) {
+			return i;
+		}
+
+		@Override
+		public View getView(int i, View view, ViewGroup viewGroup) {
+			ImageView iv;
+			if (view == null) {
+				iv = new ImageView(MainActivity.this);
+				iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+				iv.setLayoutParams(new GridView.LayoutParams(50, 50));
+			} else {
+				iv = (ImageView) view;
+			}
+			ResolveInfo info = apps.get(i);
+			iv.setImageDrawable(info.activityInfo.loadIcon(getPackageManager()));
+			return iv;
+		}
 	}
 
 	@Override
