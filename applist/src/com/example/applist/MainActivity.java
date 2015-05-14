57ababb780aa40cd3e76ce067743bab0d62e3bc1
jane0828 <janewu0828@gmail.com>
2015-05-14 10:43:29 +0800

    add

--- applist/src/com/example/applist/MainActivity.java
+++ applist/src/com/example/applist/MainActivity.java
@@ -1,81 +1,74 @@
 package com.example.applist;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
 
 public class MainActivity extends ActionBarActivity {
-
-	// 系统中所有的app信息
-	private List<ResolveInfo> apps;
-
-	// 用九宫格视图呈现所有app
-	private GridView apps_list;
+	ListView lv;
+	MyAdapter adapter;
+	ArrayList<HashMap<String, Object>> items = new ArrayList<HashMap<String, Object>>();
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		apps_list = (GridView) findViewById(R.id.apps_list);
-		loadApps();
-	}
+		lv = (ListView) findViewById(R.id.lv);
+		// 得到PackageManager对象
+		PackageManager pm = getPackageManager();
+		// 得到系统安装的所有程序包的PackageInfo对象
+		// List<ApplicationInfo> packs = pm.getInstalledApplications(0);
+		List<PackageInfo> packs = pm.getInstalledPackages(0);
 
-	private void loadApps() {
-		// 获取系统所有app信息
-		Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-		new ImageView(MainActivity.this);
-		apps = getPackageManager().queryIntentActivities(mainIntent, 0);
-		// 把信息设置到适配器中
-		apps_list.setAdapter(new AppsAdapter());
-	}
-
-	public class AppsAdapter extends BaseAdapter {
-
-		public AppsAdapter() {
-
-		}
-
-		@Override
-		public int getCount() {
-			return apps.size();
-		}
-
-		@Override
-		public Object getItem(int i) {
-			return apps.get(i);
-		}
-
-		@Override
-		public long getItemId(int i) {
-			return i;
-		}
-
-		@Override
-		public View getView(int i, View view, ViewGroup viewGroup) {
-			ImageView iv;
-			if (view == null) {
-				iv = new ImageView(MainActivity.this);
-				iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
-				iv.setLayoutParams(new GridView.LayoutParams(50, 50));
-			} else {
-				iv = (ImageView) view;
+		for ( PackageInfo pi : packs) {
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			// 显示用户安装的应用程序，而不显示系统程序
+			if ((pi.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0
+					&& (pi.applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 0) {
+				// 这将会显示所有安装的应用程序，包括系统应用程序
+				map.put("icon", pi.applicationInfo.loadIcon(pm));// 图标
+				map.put("appName", pi.applicationInfo.loadLabel(pm));// 应用程序名称
+				map.put("packageName", pi.applicationInfo.packageName);// 应用程序包名
+				// 循环读取并存到HashMap中，再增加到ArrayList上，一个HashMap就是一项
+				items.add(map);
 			}
-			ResolveInfo info = apps.get(i);
-			iv.setImageDrawable(info.activityInfo.loadIcon(getPackageManager()));
-			return iv;
+			// //这将会显示所有安装的应用程序，包括系统应用程序
+			// map.put("icon", pi.applicationInfo.loadIcon(pm));//图标
+			// map.put("appName", pi.applicationInfo.loadLabel(pm));//应用程序名称
+			// map.put("packageName", pi.applicationInfo.packageName);//应用程序包名
+			// 循环读取并存到HashMap中，再增加到ArrayList上，一个HashMap就是一项
+			// items.add(map);
 		}
+		/**
+		 * 参数：Context ArrayList(item的集合) item的layout 包含ArrayList中的HashMap的key的数组
+		 * key所对应的值的相应的控件id
+		 */
+		adapter = new MyAdapter(this, items, R.layout.piitem, new String[] {
+				"icon", "appName", "packageName" }, new int[] { R.id.icon,
+				R.id.appName, R.id.packageName });
+		lv.setAdapter(adapter);
 	}
 
 	@Override
